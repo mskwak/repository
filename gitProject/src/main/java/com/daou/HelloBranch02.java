6cee900bcf062c9155dc2c34ed42d616d850cec6
mskw <mskw@daou.co.kr>
2016-12-07 11:59:37 +0900

    no message

--- gitProject/src/main/java/com/daou/HelloBranch02.java
+++ gitProject/src/main/java/com/daou/HelloBranch02.java
@@ -4,5 +4,6 @@ public class HelloBranch02 {
 	public static void main(String[] args) {
 		System.out.println("Hello Branch02");
 		System.out.println("Hello Branch02");
+		System.out.println("Hello Branch02");
 	}
 }
